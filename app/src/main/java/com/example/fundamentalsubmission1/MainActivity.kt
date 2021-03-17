4dcedb65cc8352874aaa9581e42a3b76602b887f
Znth3 <zynthkas@gmail.com>
2021-03-17 08:07:59 +0700

    binding configuration at MainActivity.kt

--- app/src/main/java/com/example/fundamentalsubmission1/MainActivity.kt
+++ app/src/main/java/com/example/fundamentalsubmission1/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.fundamentalsubmission1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fundamentalsubmission1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
     }
