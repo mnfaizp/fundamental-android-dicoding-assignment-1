61f219e69a9b1058064f3943d69f4809bebfc8bf
Znth3 <zynthkas@gmail.com>
2021-04-14 20:10:00 +0700

    fix

--- app/src/main/java/com/example/fundamentalsubmission1/api/ApiService.kt
+++ app/src/main/java/com/example/fundamentalsubmission1/api/ApiService.kt
@@ -4,7 +4,6 @@ import com.example.fundamentalsubmission1.models.SearchUser
 import com.example.fundamentalsubmission1.models.User
 import com.example.fundamentalsubmission1.models.UserDetail
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
--- app/src/main/java/com/example/fundamentalsubmission1/viewmodels/DetailUserViewModel.kt
+++ app/src/main/java/com/example/fundamentalsubmission1/viewmodels/DetailUserViewModel.kt
@@ -1,16 +1,11 @@
 package com.example.fundamentalsubmission1.viewmodels
 
-import androidx.lifecycle.MutableLiveData
+
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
-import androidx.lifecycle.viewModelScope
-import com.example.fundamentalsubmission1.api.ApiHelper
-import com.example.fundamentalsubmission1.api.RetrofitBuilder
-import com.example.fundamentalsubmission1.models.UserDetail
 import com.example.fundamentalsubmission1.repositories.UserRepository
 import com.example.fundamentalsubmission1.utils.Resource
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import java.lang.Exception
 
 class DetailUserViewModel(private val userRepository: UserRepository) : ViewModel() {
