8a13ddc48fb6ec4e71a1b915c46f0f182cbd5b61
Znth3 <zynthkas@gmail.com>
2021-04-14 20:10:39 +0700

    make model and room dependency

---
+++ app/src/main/java/com/example/fundamentalsubmission1/models/UserFavorite.kt
@@ -0,0 +1,9 @@
+package com.example.fundamentalsubmission1.models
+
+
+data class UserFavorite(
+        val avatar_url: String,
+        val id: Int,
+        val login: String,
+        val url: String
+)
